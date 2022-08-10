99fb01139d60149b69e0e20c460a824418d3f06a
Rodrigo Niebles <rodrigo05ne@gmail.com>
2022-08-10 17:23:49 -0500

    2commit

--- Chistes_API/API/urls.py
+++ Chistes_API/API/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
+from .views import ChistesView
 from rest_framework.urlpatterns import format_suffix_patterns
-#from .views import ChistesChuk
 from API import views
 from rest_framework.views import APIView
 from API.views import chistes_list
 
 urlpatterns = [
-    path ('chistes/<int:pk>',views.chistes_list)
+    path ('chistes/<int:pk>',views.chistes_list),
+    path ('chiste/', ChistesView.as_view(), name='chiste')
 ]   
--- Chistes_API/API/views.py
+++ Chistes_API/API/views.py
@@ -1,31 +1,70 @@
-#from chunk import Chunk
 from rest_framework import status
-from urllib import response
-from django.shortcuts import render
 import requests
 from rest_framework.response import Response
-from rest_framework.views import APIView
+from django.http import HttpResponse, JsonResponse
 from API.models import Chistes
-from rest_framework.views import View
+from rest_framework.views import APIView
 from rest_framework.decorators import api_view
 from API.serializers import ChisteSerializer
 #from Chistes_API.API import serializers
+#from Chistes_API.API import serializers
+
 
 @api_view(['GET', 'POST', 'PUT', 'DELETE'])
-#class ChistesChuk(APIView):
+
 def chistes_list(request, pk):
-    try:
+    if(id):
+        r = requests.get('https://api.chucknorris.io/jokes/random')
+        return HttpResponse(r)
+
+
+class ChistesView(APIView):
+    def get (self,request):
+        obj = Chistes.objects.all()
+        serializer = ChisteSerializer(obj, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post (self,request):    
+        serializer = ChisteSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return  Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)  
+
+
+
+
+
+
+
+
+
+
+    '''try:
         chistes = Chistes.objects.get(pk=pk)
     except Chistes.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)    
 
 
     if request.method == 'GET':
-        response= requests.get ('https://api.chucknorris.io/jokes/random')
-        serializer = ChisteSerializer(chistes)
-        return Response(serializer.data)
+        response = requests.get ('https://api.chucknorris.io/jokes/random')
+        print('rodri'+response)'''
+        
 
-    elif request.method == 'PUT':
+
+
+
+
+
+
+
+
+
+
+        #serializer = ChisteSerializer(chistes)
+        #return Response(serializer.data)
+
+    '''elif request.method == 'PUT':
         serializer = ChisteSerializer(chistes, data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -34,7 +73,7 @@ def chistes_list(request, pk):
 
     elif request.method == 'DELETE':
         chistes.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)    
+        return Response(status=status.HTTP_204_NO_CONTENT) '''   
 
 
 
